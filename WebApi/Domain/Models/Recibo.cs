46e979cd76ce34b188400ab10cea6e87e00d9cc7
Carlos <carlosporrasayala@gmail.com>
2022-12-22 22:23:55 -0500

    Agregar archivos de proyecto.

---
+++ WebApi/Domain/Models/Recibo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Domain.Models
+{
+    public class Recibo
+    {
+        public int Id { get; set; }
+
+        public string Logo { get; set; }
+        [Required]
+        public int TipoMoneda { get; set; }
+        [Required]
+        public double Monto { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public string Titulo { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(150)")]
+        public string Descripcion { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(150)")]
+        public string Direccion { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(150)")]
+        public string Nombres { get; set; }
+        [Required]
+        public int TipoDocumento { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public string NumeroDocumento { get; set; } 
+    }
+}
